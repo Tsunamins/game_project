6d56835e6a040882077af9b00ad78f35362785e2
Tsunamins <reillyamr@gmail.com>
2019-12-14 14:13:42 -0500

    seed item db with possible inventory items

--- game_back/db/seeds.rb
+++ game_back/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item.create([
+{name: 'Wine Bottle', description: 'A nice bordeaux', image_file: 'winebottle.png'},
+{name: 'Yoga Mat', description: 'An expensive yoga mat', image_file: 'yogamat.png'},
+{name: 'Gift', description: 'A mystery gift', image_file: 'gift.png'},
+{name: 'Sailboat', description: 'A sailboat voucher for free lesson', image_file: 'sailboat.png'}
+])
